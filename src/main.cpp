5d35bc9a581d7987fd0ccafddbe223ac109fa971
Harshit <harshit011106@gmail.com>
2025-09-04 00:04:27 +0530

    Added a Uniform named 'Scale'

--- src/main.cpp
+++ src/main.cpp
@@ -31,12 +31,12 @@ void display(GLFWwindow* window, double currentTime){
 // Vertices coordinates
 GLfloat vertices[] =
 { //               COORDINATES                  /     COLORS           //
-	-0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower left corner
-	 0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower right corner
-	 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     1.0f, 0.6f,  0.32f, // Upper corner
-	-0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner left
-	 0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner right
-	 0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f  // Inner down
+	-0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.0f, 1.0f,  0.00f, // Lower left corner
+	 0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.0f, 0.0f,  1.0f, // Lower right corner
+	 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     1.0f, 0.0f,  0.0f, // Upper corner
+	-0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.5f, 0.5f,  0.0f, // Inner left
+	 0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.5f, 0.0f,  0.5f, // Inner right
+	 0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.0f, 0.5f,  0.5f  // Inner down
 };
 
 // Indices for vertices order
@@ -127,6 +127,8 @@ int main() {
     VBO1.Unbind();
     EBO1.Unbind();
     
+    GLuint uniID = glGetUniformLocation(shaderProgram.ID,"scale");
+
 
     // Step 3: Render loop (draw frame repeatedly until window closes)
 
@@ -143,6 +145,9 @@ int main() {
         
         // 2. Use shader
         shaderProgram.Activate();
+
+        glUniform1f(uniID, 0.5);
+
         // 3. Bind VAO
         VAO1.Bind();
 
